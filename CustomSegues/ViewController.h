e0d1d1d257fd0f934db23c5d9b06d48600723b06
Kevin Remigio <remigiokevin@gmail.com>
2016-04-07 23:00:50 -0500

    Initial Commit

---
+++ CustomSegues/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomSegues
+//
+//  Created by Kevin Remigio on 4/7/16.
+//  Copyright © 2016 Kevin Remigio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CustomSegues/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomSegues
+//
+//  Created by Kevin Remigio on 4/7/16.
+//  Copyright © 2016 Kevin Remigio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
